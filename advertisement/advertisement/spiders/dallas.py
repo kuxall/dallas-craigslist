e7234d57e48e5c09ecab76dc6c2b68ec3e76ec0f
kuxall <kuxall0@gmail.com>
2022-06-17 12:31:25 +0545

    Upload title, url, and time in csv

--- advertisement/advertisement/spiders/dallas.py
+++ advertisement/advertisement/spiders/dallas.py
@@ -1,16 +1,33 @@
 import scrapy
+from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
 
 
 class DallasSpider(scrapy.Spider):
     name = 'titles'
-    allowed_domains = ['https://dallas.craigslist.org/search/jjj']
-    start_urls = ['http://dallas.craigslist.org/search/jjj/']
+    allowed_domains = ['dallas.craigslist.org']
+    start_urls = [
+        'https://dallas.craigslist.org/search/jjj',
+    ]
 
     def parse(self, response):
-       job = {}
-       job['url'] = response.css('a.result-title::attr(href)').get()
-       return job
+        for jobs in response.css('div.result-info'):
+            yield {
+                'title': jobs.css('h3.result-heading a::text').get(),
+                'url': jobs.css('h3.result-heading a::attr(href)').get(),
+                'time': jobs.css('time.result-date::text').get()
+            }
+
+        # link of the next  button
+        next_page = response.css('a.button.next::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+        # pass
+        # allAds = response.css("div.result-info")
+
+        # for ad in allAds:
         # yield{
-        #     'link': response.xpath("//a/@href").extract(),
-        #     'posted-date': response.xpath('//*[@id="search-results"]/li[1]/div/time/text()').getall(),
+        #     'date' : response.css("time::text").get(),
+        #     'link' : response.css('a.result-title::attr(href)').get(),
+        #     # "link" : response.css("a::attr(href)").get(),
         # }
